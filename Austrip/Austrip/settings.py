4bf0cfc58447e86eb73b4ffc44ab12c8bd333e90
Tsz yin Tsoi <ttso7897@uni.sydney.edu.au>
2020-11-09 11:59:21 +1100

    RESTapi files
    Added routes to RESTapi, Authentication to be added in next commit

--- Austrip/Austrip/settings.py
+++ Austrip/Austrip/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'accounts',
+    'rest_framework',
+    'rest_framework.authtoken',
+
 ]
 
 MIDDLEWARE = [
--- Austrip/main/views.py
+++ Austrip/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+
 from .models import Destination, Attraction, DestinationComment, AttractionComment, Recommendation
+from .serializers import *
 
 
 def home(request):
@@ -22,3 +25,29 @@ def detailed_item(request, name):
 
 def destination_detail(request):
     return render(request, 'destination_detail.html')
+
+
+class DestinationModelViewSet(viewsets.ModelViewSet):
+    serializer_class = DestinationSerializer
+    queryset = Destination.objects.all()
+
+class UserModelViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class AttractionModelViewSet(viewsets.ModelViewSet):
+    serializer_class = AttractionSerializer
+    queryset = Attraction.objects.all()
+
+class DestinationCommentModelViewSet(viewsets.ModelViewSet):
+    serializer_class = DestinationCommentSerializer
+    queryset = DestinationComment.objects.all()
+
+class AttractionCommentModelViewSet(viewsets.ModelViewSet):
+    serializer_class = AttractionCommentSerializer
+    queryset = AttractionComment.objects.all()
+
+class RecommendationModelViewSet(viewsets.ModelViewSet):
+    serializer_class = RecommendationSerializer
+    queryset = Recommendation.objects.all()
+
