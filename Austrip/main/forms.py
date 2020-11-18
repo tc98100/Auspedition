e533ed9ea25f9c844bcb0626ebe6a79a44ecb89a
Joseph Nguyen <jngu2185@uni.sydney.edu.au>
2020-11-19 02:41:42 +1100

    redesigned edit profile and change password

--- Austrip/main/forms.py
+++ Austrip/main/forms.py
@@ -42,10 +42,3 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'password1', 'password2']
-        # widgets = {
-        #     'username': forms.TextInput(attrs={'class': 'input', 'placeholder': 'Enter Username'}),
-        #     'first_name': forms.TextInput(attrs={'class': 'input', 'placeholder': 'Enter Firstname'}),
-        #     'last_name': forms.TextInput(attrs={'class': 'input'}),
-        #     'password1': forms.TextInput(attrs={'class': 'input'}),
-        #     'password2': forms.TextInput(attrs={'class': 'input'}),
-        # }
